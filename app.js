97d4b3546c0737df286b9c715e81a0439f5f218e
ahmedalzarka <ahmeh.alzarka@gmail.com>
2023-05-28 11:52:23 +0300

    working

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ btn.addEventListener('click',() => {
 function createNotification(){
     const notification = document.createElement("div")
     notification.classList.add('notification')
-
+    notification.innerHTML = `
+        <div class="logo-info">
+            <img src="facebook.png" alt="facebook">
+            <h2>Facebook</h2>
+            <h3>9m ago</h3>
+        </div>
+        <h1>friend request!</h1>
+        <h3><span>Ahmed</span> sent you a friend request!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        `
     container.appendChild(notification)
-    
+
+    setTimeout(() => {
+        notification.remove();
+    },4000)
 }
